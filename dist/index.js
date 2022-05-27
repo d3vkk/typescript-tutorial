c9dc7df26074099feb2d602762f7fe6f9e4acf16
d3vkk <donaldkk22@yahoo.com>
2022-05-27 17:07:48 +0300

    feat: Arrays and tuples

--- dist/index.js
+++ dist/index.js
@@ -6,3 +6,14 @@ let isPublished = true;
 let x = "Hello";
 // Doesn't show errors with a reassignment of type
 x = true;
+// Arrays
+let ids = [6, 5, 8, 4, 5];
+let arr = [5, true, "Bye World"];
+// Tuples - specify multiple types in a specific order
+let person = [9, "Country", false];
+// Tuple Array
+let employee = [
+    [1, "John"],
+    [2, "Sarah"],
+    [3, "Connor"],
+];
